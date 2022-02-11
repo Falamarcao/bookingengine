4770d88185884b75eb0a8d54363ca757303e621d
falamarcao <falamarcao@gmail.com>
2022-02-11 13:09:40 -0300

    some logic corrections

--- listings/views.py
+++ listings/views.py
@@ -4,7 +4,6 @@ from .utils import str_to_datetime
 from django.db.models import Min
 from rest_framework import response, serializers, views
 
-import traceback
 
 class Units(views.APIView):
     """
@@ -24,19 +23,19 @@ class Units(views.APIView):
             # Query URL params
             check_in = str_to_datetime(self.request.query_params.get('check_in', None))
             check_out = str_to_datetime(self.request.query_params.get('check_out', None))
-            max_price = int(self.request.query_params.get('max_price', None))
+            max_price = self.request.query_params.get('max_price', None)
 
             # Checking reservations from requested date range
-            reservations_ids = Reservation.objects.filter(check_in__range=(check_in, check_out)).values('id')                                                          \
+            reservations_ids = Reservation.objects.filter(check_in__range=(check_in, check_out)).values('booking_info__id')                                                          \
                            &   \
-                           Reservation.objects.filter(check_out__range=(check_in, check_out)).values('id')
+                           Reservation.objects.filter(check_out__range=(check_in, check_out)).values('booking_info__id')
 
             # Excluding blocked places
             booking_info = BookingInfo.objects.exclude(id__in=reservations_ids)
 
             # max_price filter
             if max_price:
-                booking_info = booking_info.filter(price__lte=max_price)
+                booking_info = booking_info.filter(price__lte=int(max_price))
 
             # Only the cheapest HotelRoomType with available HotelRoom
             cheapest = booking_info.values('hotel_room_type__hotel').annotate(Min('price'))
@@ -47,5 +46,8 @@ class Units(views.APIView):
 
             return response.Response({'items': BookingInfoSerializer(booking_info, many=True).data})
        
+        except ValueError:
+            raise serializers.ValidationError('Value Error, you should check the URL params.')
+
         except Exception as e:
             raise serializers.ValidationError(e)
